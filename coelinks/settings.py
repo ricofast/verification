e3e20e9b31ab8ec4bc78c9e7c453c8d9e611f42d
opencourse2020 <akaaboune@gmail.com>
2023-06-12 20:54:32 -0500

    first commit

--- coelinks/settings.py
+++ coelinks/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env.str("DJANGO_SECRET_KEY", default="!!!SET DJANGO_SECRET_KEY!!!",
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 ADMIN_URL = 'admin/'
 
 # Application definition
