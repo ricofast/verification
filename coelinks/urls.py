d2dd8c620c153d7e216b60e90ef1d9b79ada7292
opencourse2020 <akaaboune@gmail.com>
2023-07-15 04:46:13 -0500

    chatbot first commit

--- coelinks/urls.py
+++ coelinks/urls.py
@@ -18,9 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+from api.verify.views import FileUpdateView, DocumentScanView
+from api.chatbot.views import ChatGenerateView
+
+router = DefaultRouter()
+
+router.register(r"verify", FileUpdateView)
+router.register(r"chat", ChatGenerateView)
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-    path('api/verify', include('api.verify.urls', namespace='verify')),
-    path('api/chat', include('api.chatbot.urls', namespace='chat')),
+    # path('api/verify', include('api.verify.urls', namespace='verify')),
+    # path('api/chat', include('api.chatbot.urls', namespace='chat')),
+    path("api/", include(router.urls))
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
