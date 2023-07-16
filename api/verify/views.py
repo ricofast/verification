a23f32086f15ecd7c254ee162d9c13552ca5a739
opencourse2020 <akaaboune@gmail.com>
2023-07-16 10:21:59 -0500

    chatbot first commit

--- api/verify/views.py
+++ api/verify/views.py
@@ -21,7 +21,7 @@ import torch
 import torchvision.transforms as transforms
 import PIL.Image as Image
 from . import RRDBNet_arch as arch
-
+import os.path as osp
 # import easyocr
 
 classes = [
@@ -51,6 +51,39 @@ def set_device():
   else:
     dev = "cpu"
   return torch.device(dev)
+
+def enhancepictures(userid):
+  model_path = picture_enhance_model  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
+  device = torch.device('cpu')  # if you want to run on CPU, change 'cuda' -> cpu
+  # device = torch.device('cpu')
+
+  test_img_folder = os.getcwd() + "/media/images/user_" + str(userid) + "/*"
+  test_user_folder = os.getcwd() + "/media/images/user_" + str(userid) + "/"
+
+  model = arch.RRDBNet(3, 3, 64, 23, gc=32)
+  model.load_state_dict(torch.load(model_path), strict=True)
+  model.eval()
+  model = model.to(device)
+
+  print('Model path {:s}. \nTesting...'.format(model_path))
+
+  idx = 0
+  for path in glob.glob(test_img_folder):
+    idx += 1
+    base = osp.splitext(osp.basename(path))[0]
+    print(idx, base)
+    # read images
+    img = cv2.imread(path, cv2.IMREAD_COLOR)
+    img = img * 1.0 / 255
+    img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+    img_LR = img.unsqueeze(0)
+    img_LR = img_LR.to(device)
+
+    with torch.no_grad():
+      output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+    output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
+    output = (output * 255.0).round()
+    cv2.imwrite('{pth}{file}_rlt.png'.format(pth=test_user_folder, file=base), output)
 
 
 def Enhancepicture(athname, userid):
@@ -122,7 +155,7 @@ class FileUpdateView(APIView):
       filename = file_serializer.validated_data['file']
 
       print("step 1")
-      Enhancepicture(filename, userid)
+      enhancepictures(userid)
       print("step 2")
       verified = classify(ai_model, image_transforms, filename, classes)
       if verified != "Invalid":
