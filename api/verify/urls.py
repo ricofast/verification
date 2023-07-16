580009fd239a30129a547d8af7408b29fd79ee09
opencourse2020 <akaaboune@gmail.com>
2023-07-16 09:27:21 -0500

    chatbot first commit

--- api/verify/urls.py
+++ api/verify/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "verify"
 
 urlpatterns = [
-    re_path('', views.FileUpdateView.as_view(), name='file-upload'),
+    re_path('', views.DocumentScanView.as_view(), name='file-upload'),
     re_path('scan/', views.DocumentScanView.as_view(), name='file-scan'),
 ]
