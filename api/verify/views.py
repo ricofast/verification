647a4157ae4cd19502a78413d74c3bf31b83bf29
opencourse2020 <akaaboune@gmail.com>
2023-07-19 05:52:37 -0500

    chatbot first commit

--- api/verify/views.py
+++ api/verify/views.py
@@ -210,9 +210,9 @@ class PictureVerifyView(APIView):
         user=userid,
         defaults={'keyword': key_word, 'file': filename},
       )
-      is_clear = True
-      print("File path: " + obj.file.path)
-      # is_clear = is_head_shot_clear(obj.file.path)
+      # is_clear = True
+      # print("File path: " + obj.file.path)
+      is_clear = is_head_shot_clear(obj.file.path)
       if is_clear:
         verified = "clear"
       else:
