ce09caa4f2eb2dfd63753fd0c6ab465e79201bc8
opencourse2020 <akaaboune@gmail.com>
2023-08-17 10:19:28 -0500

    chatbot first commit

--- api/verify/cron.py
+++ api/verify/cron.py
@@ -3,8 +3,8 @@ from .models import Document, HeadShot
 
 def document_ocr():
     docs = Document.objects.filter(verified=True, scanned=True)
-    verified_ids = ""
-    for doc in docs:
-        verified_ids = doc.pk
-    print(verified_ids)
+    with open('verifiedids.txt', 'a') as f:
+        for doc in docs:
+            f.writelines(doc.pk)
+
     return
