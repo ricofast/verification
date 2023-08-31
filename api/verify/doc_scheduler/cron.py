69e49363be34f98dda6a40df7d898f10d6568574
opencourse2020 <akaaboune@gmail.com>
2023-08-31 15:22:18 -0500

    chatbot first commit

--- api/verify/doc_scheduler/cron.py
+++ api/verify/doc_scheduler/cron.py
@@ -1,5 +1,10 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from api.verify.models import Document
+from api.verify.views import preprocess_image
+import keras_ocr
+import pandas as pd
+import os
+import glob
 
 # def document_ocr():
 #     # docs = Document.objects.filter(verified=True, scanned=True)
@@ -13,9 +18,26 @@ from api.verify.models import Document
 scheduler = BackgroundScheduler()
 
 def period():
-    docs = Document.objects.all()
+    docs = Document.objects.filter(verified=True, scanned=True).first()
     # doc = Document.objects.create(user=19, keyword='Test 22')
-    print(docs)
+    userid = docs.pk
+    kw = docs.keyword
+    print("user Id:")
+    print(userid)
+    print(kw)
+    path = os.getcwd() + "/media/documents/user_" + str(userid) + "/*"
+    status = ""
+    pipeline = keras_ocr.pipeline.Pipeline()
+    for path_to_document in glob.glob(path, recursive=True):
+        images = [keras_ocr.tools.read(img) for img in [path_to_document]]
+        prediction_groups = pipeline.recognize(images)
+        df = pd.DataFrame(prediction_groups[0], columns=['text', 'bbox'])
+        nameexist = kw in df['text'].values
+        if nameexist:
+            status = "Verified"
+
+    print(status)
+
 
 def start():
     scheduler.add_job(period, "interval", minutes=1, id="unverifiedusers_001",
