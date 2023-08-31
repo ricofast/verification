2e12a53c59a92906479995b0ddedd73736b6cc88
opencourse2020 <akaaboune@gmail.com>
2023-08-31 14:34:14 -0500

    chatbot first commit

--- api/verify/apps.py
+++ api/verify/apps.py
@@ -1,10 +1,12 @@
 from django.apps import AppConfig
-from api.verify.doc_scheduler import cron
+
 
 class VerifyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api.verify'
 
     def ready(self):
+
         print("Starting Scheduler ...")
+        from api.verify.doc_scheduler import cron
         cron.start()
