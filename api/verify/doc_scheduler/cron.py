5e7a6f149928455d60fe9077ab9572288be3ae14
opencourse2020 <akaaboune@gmail.com>
2023-10-07 07:59:18 -0500

    chatbot first commit

--- api/verify/doc_scheduler/cron.py
+++ api/verify/doc_scheduler/cron.py
@@ -5,6 +5,7 @@ import keras_ocr
 import pandas as pd
 import os
 import glob
+import shutil
 
 # def document_ocr():
 #     # docs = Document.objects.filter(verified=True, scanned=True)
@@ -18,28 +19,44 @@ import glob
 scheduler = BackgroundScheduler()
 
 def period():
-    docs = Document.objects.filter(verified=True, scanned=False)
+    docs = Document.objects.filter(verified=True, scanned=False).values("user", "keyword")
     # doc = Document.objects.create(user=19, keyword='Test 22')
-
-    for doc in docs:
-        userid = doc.user
-        kw = doc.keyword
-        print("user Id:")
-        print(userid)
-        print(kw)
-        path = os.getcwd() + "/media/documents/user_" + str(userid) + "/*"
-        status = ""
-        pipeline = keras_ocr.pipeline.Pipeline()
+    df_docs = pd.DataFrame(list(docs))
+    path_of_docs = []
+    for ind in df_docs.index:
+        path = os.getcwd() + "/media/documents/user_" + str(df_docs["user"][ind]) + "/*"
         for path_to_document in glob.glob(path, recursive=True):
-            images = [keras_ocr.tools.read(img) for img in [path_to_document]]
-            prediction_groups = pipeline.recognize(images)
-            df = pd.DataFrame(prediction_groups[0], columns=['text', 'bbox'])
-            nameexist = kw in df['text'].values
-            if nameexist:
-                status = "Verified"
-                with open('verifieds.txt', 'a') as f:
-                    f.writelines(docs.user)
+            path_of_docs = [path_of_docs.append(img) for img in [path_to_document]]
+    pipeline = keras_ocr.pipeline.Pipeline()
+    images = [keras_ocr.tools.read(img) for img in [path_of_docs]]
+    prediction_groups = pipeline.recognize(images)
+    df = pd.DataFrame(prediction_groups, columns=['text', 'bbox'])
+    status = ""
+    for i in range(len(df)):
+        kw = df_docs.loc[i, "keyword"]
+        nameexist = kw in df.loc[i, 'text']
+        if nameexist:
+            status = "Verified"
+            with open('verifieds.txt', 'a') as f:
+                f.writelines(df_docs.loc[i, "user"])
+
+
         print(status)
+    # for doc in docs:
+    #     userid = doc.user
+    #     kw = doc.keyword
+    #     df_doc1 = pd.DataFrame({"userid": [userid], "keyword": [kw]})
+    #     df_doc2
+    #     print("user Id:")
+    #     print(userid)
+    #     print(kw)
+    #     path = os.getcwd() + "/media/documents/user_" + str(userid) + "/*"
+
+
+        # for path_to_document in glob.glob(path, recursive=True):
+
+
+
 
 
 
