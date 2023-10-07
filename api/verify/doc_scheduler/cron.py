bd29ca848fa1c6ec1a998070f4e2b6a8a6faa38b
opencourse2020 <akaaboune@gmail.com>
2023-10-07 07:04:45 -0500

    chatbot first commit

--- api/verify/doc_scheduler/cron.py
+++ api/verify/doc_scheduler/cron.py
@@ -18,26 +18,28 @@ import glob
 scheduler = BackgroundScheduler()
 
 def period():
-    docs = Document.objects.filter(verified=True, scanned=False).first()
+    docs = Document.objects.filter(verified=True, scanned=False)
     # doc = Document.objects.create(user=19, keyword='Test 22')
-    userid = docs.user
-    kw = docs.keyword
-    print("user Id:")
-    print(userid)
-    print(kw)
-    path = os.getcwd() + "/media/documents/user_" + str(userid) + "/*"
-    status = ""
-    pipeline = keras_ocr.pipeline.Pipeline()
-    for path_to_document in glob.glob(path, recursive=True):
-        images = [keras_ocr.tools.read(img) for img in [path_to_document]]
-        prediction_groups = pipeline.recognize(images)
-        df = pd.DataFrame(prediction_groups[0], columns=['text', 'bbox'])
-        nameexist = kw in df['text'].values
-        if nameexist:
-            status = "Verified"
-            with open('verifieds.txt', 'a') as f:
-                f.writelines(docs.user)
-    print(status)
+
+    for doc in docs:
+        userid = doc.user
+        kw = doc.keyword
+        print("user Id:")
+        print(userid)
+        print(kw)
+        path = os.getcwd() + "/media/documents/user_" + str(userid) + "/*"
+        status = ""
+        pipeline = keras_ocr.pipeline.Pipeline()
+        for path_to_document in glob.glob(path, recursive=True):
+            images = [keras_ocr.tools.read(img) for img in [path_to_document]]
+            prediction_groups = pipeline.recognize(images)
+            df = pd.DataFrame(prediction_groups[0], columns=['text', 'bbox'])
+            nameexist = kw in df['text'].values
+            if nameexist:
+                status = "Verified"
+                with open('verifieds.txt', 'a') as f:
+                    f.writelines(docs.user)
+        print(status)
 
 
 
