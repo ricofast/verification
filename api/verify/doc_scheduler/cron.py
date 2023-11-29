b868c29e2bbdd71ffa15587275a5ea43b6d68d38
opencourse2020 <akaaboune@gmail.com>
2023-11-29 07:08:25 -0600

    Change request path

--- api/verify/doc_scheduler/cron.py
+++ api/verify/doc_scheduler/cron.py
@@ -36,10 +36,12 @@ def period():
 
     all_docs = Document.objects.filter(verified=True)
     doc = all_docs.filter((Q(name__isnull=False) & Q(name_checked=False)) | (Q(dob__isnull=False) & Q(dob_checked=False)))
-    doc_name = all_docs.filter(name__isnull=False, name_checked=False).values("user", "name", "file")
+    doc_name_dob = doc.values("user", "name", "dob", "file", "name_checked", "dob_checked")
+    doc_name = all_docs.filter(name__isnull=False, name_checked=False).values("user", "name", "file").values("user", "name", "file")
     doc_dbo = all_docs.filter(dob__isnull=False, dob_checked=False).values("user", "dob", "file")
 
-    # docs = doc.values("user", "keyword", "file")
+    df_doc = pd.DataFrame(list(doc))
+    df_docs_name_dob = pd.DataFrame(list(doc_name_dob))
     df_docs_name = pd.DataFrame(list(doc_name))
     df_docs_dob = pd.DataFrame(list(doc_dbo))
 
@@ -66,144 +68,199 @@ def period():
 
         images = [keras_ocr.tools.read(img) for img in path_of_docs]
         prediction_groups = pipeline.recognize(images)
-        status = {"user":[],
-                  "passed":[]}
-        for j in range(len(prediction_groups)):
-            df = pd.DataFrame(prediction_groups[j], columns=['text', 'bbox'])
-            userid = df_docs.loc[j, "user"]
-            kw = df_docs.loc[j, "name"]
-            if kw:
-                # check keyword with multiple words
-                words = kw.split()
-                allkeywords_status = False
-                for wd in words:
-                    nameexist = wd in df['text'].values
-                    if nameexist:
-                        allkeywords_status = True
-                    else:
-                        lwd = wd.lower()
-                        similar = difflib.get_close_matches(lwd, df['text'].values)
-                        if len(similar) > 0:
-                            allkeywords_status = True
-                        else:
-                            allkeywords_status = False
-                            break
-                # for i in range(len(df)-1):
-                #     kw = df_docs.loc[j, "keyword"]
-                #     nameexist = kw in df.loc[i, 'text']
-                # nameexist = kw in df['text'].values
-                if allkeywords_status:
-
-                    payload = {
-                        "user": userid,
-                        "result": "1"
-                    }
-
-                    result = requests.post(url, data=json.dumps(payload), headers=header)
-
-                    if result.status_code == 200:
-                        status["user"].append(str(userid))
-                        status["passed"].append("Yes")
-                        obj, created = Document.objects.update_or_create(
-                            user=userid, defaults={'name_checked': True, 'name_received': True},)
-                else:
-                    payload = {
-                        "user": userid,
-                        "result": "2"
-                    }
-
-                    result = requests.post(url, data=json.dumps(payload), headers=header)
-                    if result.status_code == 200:
-                        status["user"].append(str(userid))
-                        status["passed"].append("No")
-                    # with open('verifieds.json', 'a') as f:
-                    #     f.write(str(userid))
-                    #     f.write('\n')
-                # else:
-                #     status.append("Unverified")
-
-        dt = datetime.datetime.now()
-        seq = int(dt.strftime("%Y%m%d%H%M%S"))
-        filename = f'media/verified/verified-name-{seq}.json'
-        with open(filename, 'w') as f:
-            json.dump(status, f)
-
-    if df_docs_dob.size > 0:
-        df_docs = df_docs_dob
-        images = []
-        print(df_docs)
-
-        path_of_docs = []
-        for ind in df_docs.index:
-            # path = os.getcwd() + "/media/documents/user_" + str(df_docs["user"][ind]) + "/*"
-            path = os.getcwd() + "/media/" + df_docs["file"][ind]
-            path_of_docs.append(path)
-            # for path_to_document in glob.glob(path, recursive=True):
-            #     path_of_docs = path_of_docs.append(path_to_document)
-        print(path_of_docs)
-        # print(os.getcwd())
-
-        images = [keras_ocr.tools.read(img) for img in path_of_docs]
-        prediction_groups = pipeline.recognize(images)
         status = {"user": [],
-                  "passed": []}
+                  "Name": [], "DOB": []}
         for j in range(len(prediction_groups)):
             df = pd.DataFrame(prediction_groups[j], columns=['text', 'bbox'])
+            dobchecked = df_docs.loc[j, "dob_checked"]
+            namechecked = df_docs.loc[j, "name_checked"]
             userid = df_docs.loc[j, "user"]
-            kw = df_docs.loc[j, "dob"]
-            if kw:
-                # check keyword with multiple words
-                words = kw.split()
-                allkeywords_status = False
-                for wd in words:
-                    nameexist = wd in df['text'].values
-                    if nameexist:
-                        allkeywords_status = True
-                    else:
-                        lwd = wd.lower()
-                        similar = difflib.get_close_matches(lwd, df['text'].values)
-                        if len(similar) > 0:
+            if namechecked is False:
+
+                kw = df_docs.loc[j, "name"]
+                name_status = 2
+                if kw:
+                    # check keyword with multiple words
+                    words = kw.split()
+                    allkeywords_status = False
+
+                    for wd in words:
+                        nameexist = wd in df['text'].values
+                        if nameexist:
                             allkeywords_status = True
+                            name_status = 1
                         else:
-                            allkeywords_status = False
-                            break
-                # for i in range(len(df)-1):
-                #     kw = df_docs.loc[j, "keyword"]
-                #     nameexist = kw in df.loc[i, 'text']
-                # nameexist = kw in df['text'].values
-                if allkeywords_status:
+                            lwd = wd.lower()
+                            similar = difflib.get_close_matches(lwd, df['text'].values)
+                            if len(similar) > 0:
+                                allkeywords_status = True
+                                name_status = 1
+                            else:
+                                allkeywords_status = False
+                                name_status = 2
+                                break
+            else:
+                name_status = 1
+            if dobchecked is False:
+                kw = df_docs.loc[j, "dob"]
+                dob_status = 2
+                if kw:
+                    # check keyword with multiple words
+                    words = kw.split()
+                    allkeywords_status = False
 
-                    payload = {
-                        "user": userid,
-                        "result": "3"
-                    }
+                    for wd in words:
+                        nameexist = wd in df['text'].values
+                        if nameexist:
+                            allkeywords_status = True
+                            dob_status = 1
+                        else:
+                            lwd = wd.lower()
+                            similar = difflib.get_close_matches(lwd, df['text'].values)
+                            if len(similar) > 0:
+                                allkeywords_status = True
+                                dob_status = 1
+                            else:
+                                allkeywords_status = False
+                                dob_status = 2
+                                break
+            else:
+                dob_status = 1
 
-                    result = requests.post(url, data=json.dumps(payload), headers=header)
 
-                    if result.status_code == 200:
-                        status["user"].append(str(userid))
-                        status["passed"].append("Yes")
-                        obj, created = Document.objects.update_or_create(
-                            user=userid, defaults={'dob_checked': True, 'dob_received': True}, )
-                else:
-                    payload = {
-                        "user": userid,
-                        "result": "4"
-                    }
-                    result = requests.post(url, data=json.dumps(payload), headers=header)
-                    if result.status_code == 200:
-                        status["user"].append(str(userid))
-                        status["passed"].append("No")
-                    # with open('verifieds.json', 'a') as f:
-                    #     f.write(str(userid))
-                    #     f.write('\n')
-                # else:
-                #     status.append("Unverified")
+            if dob_status == 1 and name_status == 1:
+                payload = {
+                    "user": userid,
+                    "result": 1
+                }
+
+                result = requests.post(url, data=json.dumps(payload), headers=header)
+
+                if result.status_code == 200:
+                    status["user"].append(str(userid))
+                    status["Name"].append("Yes")
+                    status["DOB"].append("Yes")
+                    obj, created = Document.objects.update_or_create(
+                        user=userid, defaults={'name_checked': True, 'dob_checked': True, 'name_received': True, 'dob_received': True}, )
+
+            elif dob_status == 1 and name_status == 2:
+                payload = {
+                    "user": userid,
+                    "result": 2
+                }
+
+                result = requests.post(url, data=json.dumps(payload), headers=header)
+                if result.status_code == 200:
+                    status["user"].append(str(userid))
+                    status["Name"].append("No")
+                    status["DOB"].append("Yes")
+            elif dob_status == 2 and name_status == 1:
+                payload = {
+                    "user": userid,
+                    "result": 3
+                }
+
+                result = requests.post(url, data=json.dumps(payload), headers=header)
+                if result.status_code == 200:
+                    status["user"].append(str(userid))
+                    status["Name"].append("Yes")
+                    status["DOB"].append("No")
+
+
+            else:
+                payload = {
+                    "user": userid,
+                    "result": 4
+                }
+
+                result = requests.post(url, data=json.dumps(payload), headers=header)
+                if result.status_code == 200:
+                    status["user"].append(str(userid))
+                    status["Name"].append("No")
+                    status["DOB"].append("No")
+
         dt = datetime.datetime.now()
         seq = int(dt.strftime("%Y%m%d%H%M%S"))
-        filename = f'media/verified/verified-dob-{seq}.json'
+        filename = f'media/verified/verified-athlete-{seq}.json'
         with open(filename, 'w') as f:
             json.dump(status, f)
+
+    # if df_docs_dob.size > 0:
+    #     df_docs = df_docs_dob
+    #     images = []
+    #     print(df_docs)
+    #
+    #     path_of_docs = []
+    #     for ind in df_docs.index:
+    #         # path = os.getcwd() + "/media/documents/user_" + str(df_docs["user"][ind]) + "/*"
+    #         path = os.getcwd() + "/media/" + df_docs["file"][ind]
+    #         path_of_docs.append(path)
+    #         # for path_to_document in glob.glob(path, recursive=True):
+    #         #     path_of_docs = path_of_docs.append(path_to_document)
+    #     print(path_of_docs)
+    #     # print(os.getcwd())
+    #
+    #     images = [keras_ocr.tools.read(img) for img in path_of_docs]
+    #     prediction_groups = pipeline.recognize(images)
+    #     status = {"user": [],
+    #               "passed": []}
+    #     for j in range(len(prediction_groups)):
+    #         df = pd.DataFrame(prediction_groups[j], columns=['text', 'bbox'])
+    #         userid = df_docs.loc[j, "user"]
+    #         kw = df_docs.loc[j, "dob"]
+    #         if kw:
+    #             # check keyword with multiple words
+    #             words = kw.split()
+    #             allkeywords_status = False
+    #             for wd in words:
+    #                 nameexist = wd in df['text'].values
+    #                 if nameexist:
+    #                     allkeywords_status = True
+    #                 else:
+    #                     lwd = wd.lower()
+    #                     similar = difflib.get_close_matches(lwd, df['text'].values)
+    #                     if len(similar) > 0:
+    #                         allkeywords_status = True
+    #                     else:
+    #                         allkeywords_status = False
+    #                         break
+    #             # for i in range(len(df)-1):
+    #             #     kw = df_docs.loc[j, "keyword"]
+    #             #     nameexist = kw in df.loc[i, 'text']
+    #             # nameexist = kw in df['text'].values
+    #             if allkeywords_status:
+    #
+    #                 payload = {
+    #                     "user": userid,
+    #                     "result": "3"
+    #                 }
+    #
+    #                 result = requests.post(url, data=json.dumps(payload), headers=header)
+    #
+    #                 if result.status_code == 200:
+    #                     status["user"].append(str(userid))
+    #                     status["passed"].append("Yes")
+    #                     obj, created = Document.objects.update_or_create(
+    #                         user=userid, defaults={'dob_checked': True, 'dob_received': True}, )
+    #             else:
+    #                 payload = {
+    #                     "user": userid,
+    #                     "result": "4"
+    #                 }
+    #                 result = requests.post(url, data=json.dumps(payload), headers=header)
+    #                 if result.status_code == 200:
+    #                     status["user"].append(str(userid))
+    #                     status["passed"].append("No")
+    #                 # with open('verifieds.json', 'a') as f:
+    #                 #     f.write(str(userid))
+    #                 #     f.write('\n')
+    #             # else:
+    #             #     status.append("Unverified")
+    #     dt = datetime.datetime.now()
+    #     seq = int(dt.strftime("%Y%m%d%H%M%S"))
+    #     filename = f'media/verified/verified-dob-{seq}.json'
+    #     with open(filename, 'w') as f:
+    #         json.dump(status, f)
 
 def start():
     scheduler.add_job(period, "interval", hours=2, id="unverifiedusers_001",
