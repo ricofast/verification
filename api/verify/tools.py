14af2cb51d38867df0ade75056596a6716966f63
opencourse2020 <akaaboune@gmail.com>
2023-12-15 06:36:45 -0600

    Change request path

--- api/verify/tools.py
+++ api/verify/tools.py
@@ -146,6 +146,7 @@ def headshots_count(image_path):
     # nfc = check_face(image1)
     # if nfc > 0:
       verified = 1
+      torch.cuda.empty_cache()
       # device = cuda.get_current_device()
       # device.reset()
   # elif one_person == 1 and not class_id[0] == 0:
